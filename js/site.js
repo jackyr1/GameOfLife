aa486ca92c9fdf4b9c0ecd351ed9d9a5590d6f0a
jackyr1 <103156038+jackyr1@users.noreply.github.com>
2024-05-20 22:08:53 -0700

    more button messing around

--- js/site.js
+++ js/site.js
@@ -1,10 +1,49 @@
 
-
+//ucsc big word title
 $("#output").html('<div class="text2"><p>' + "UCSC" + '</p></div>');
 
-
-// add a click listener to the all buttons and toggle special effect
+//hides and makes info appear - this isn't working it may not be connected idk
 $("#porter").click(function(){
-    // now add (or subtract) the "special" class to the section
-    $(this).toggleClass("special");
-});
+    $(this).toggle('slow');
+    $("#hideP").toggleClass("hidden");
+})  
+
+$("#rC").click(function(){
+    $(this).toggle('slow');
+    $("#hideR").toggleClass("hidden");
+})  
+
+$("#oakes").click(function(){
+    $(this).toggle('slow');
+    $("#hideO").toggleClass("hidden");
+})  
+
+$("#kresge").click(function(){
+    $(this).toggle('slow');
+    $("#hideK").toggleClass("hidden");
+})  
+
+$("#jRL").click(function(){
+    $(this).toggle('slow');
+    $("#hideJ").toggleClass("hidden");
+})  
+
+$("#crown").click(function(){
+    $(this).toggle('slow');
+    $("#hideC").toggleClass("hidden");
+})  
+
+$("#merrill").click(function(){
+    $(this).toggle('slow');
+    $("#hideM").toggleClass("hidden");
+})  
+
+$("#cowell").click(function(){
+    $(this).toggle('slow');
+    $("#hideCW").toggleClass("hidden");
+})  
+
+$("#stevenson").click(function(){
+    $(this).toggle('slow');
+    $("#hideS").toggleClass("hidden");
+})  
