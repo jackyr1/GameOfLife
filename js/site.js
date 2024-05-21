71b43c084e76a2061cc5b1169824053580c0e57d
jackyr1 <103156038+jackyr1@users.noreply.github.com>
2024-05-20 21:06:52 -0700

    button for colleges :(

--- js/site.js
+++ js/site.js
@@ -1,22 +1,10 @@
 
 
+$("#output").html('<div class="text2"><p>' + "UCSC" + '</p></div>');
 
 
-
-// Sorts the characters of a string in alphabetical order.
-function sortString(inputString) {
-    // returns string
-    return inputString;
-  }
-  
-
-// click listener for button
-$("#submit").click(function(){
-    // get value of input field
-    const userName = $("#user-name").val();
-    // now let's sort it
-    userNameSorted = sortString(userName);
-    // append a new div to our output div
-    $("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
-  });
-  
+// add a click listener to the all buttons and toggle special effect
+$("#porter").click(function(){
+    // now add (or subtract) the "special" class to the section
+    $(this).toggleClass("special");
+});
