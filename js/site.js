4e6595ce79069c5355a4d530d43de07b9398ab6d
mlkincaid <mayalillian295@gmail.com>
2024-05-24 16:09:58 -0700

    button works!

--- js/site.js
+++ js/site.js
@@ -2,48 +2,10 @@
 //ucsc big word title
 $("#output").html('<div class="text2"><p>' + "UCSC" + '</p></div>');
 
-//hides and makes info appear - this isn't working it may not be connected idk
-$("#porter").click(function(){
-    $(this).toggle('slow');
-    $("#hideP").toggleClass("hidden");
-})  
-
-$("#rC").click(function(){
-    $(this).toggle('slow');
-    $("#hideR").toggleClass("hidden");
-})  
-
-$("#oakes").click(function(){
-    $(this).toggle('slow');
-    $("#hideO").toggleClass("hidden");
-})  
-
-$("#kresge").click(function(){
-    $(this).toggle('slow');
-    $("#hideK").toggleClass("hidden");
-})  
-
-$("#jRL").click(function(){
-    $(this).toggle('slow');
-    $("#hideJ").toggleClass("hidden");
-})  
-
-$("#crown").click(function(){
-    $(this).toggle('slow');
-    $("#hideC").toggleClass("hidden");
-})  
-
-$("#merrill").click(function(){
-    $(this).toggle('slow');
-    $("#hideM").toggleClass("hidden");
-})  
-
-$("#cowell").click(function(){
-    $(this).toggle('slow');
-    $("#hideCW").toggleClass("hidden");
-})  
-
-$("#stevenson").click(function(){
-    $(this).toggle('slow');
-    $("#hideS").toggleClass("hidden");
+//hides and makes info appear
+$("#start").click(function(){
+   // gets rid of first page
+    $("#startpage").addClass("hidden");
+    // brings in day1
+    $("#day1").removeClass("hidden");
 })  
