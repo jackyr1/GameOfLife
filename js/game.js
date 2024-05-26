ccd66e675acebf0a9a83ae41747bd0475958c15e
mlkincaid <mayalillian295@gmail.com>
2024-05-26 16:58:32 -0700

    college stuff

--- js/game.js
+++ js/game.js
@@ -7,28 +7,53 @@ const sections = {
         {
             content: (
                 <div>
-                    <h2>Welcome to Stevenson College</h2>
+                    <h2>Welcome to Stevenson College!</h2>
                     <img class="stevenson"
                         src="./img/stevensondorms.webp"
                         alt="Stevenson college at UCSC dorms">
                     </img>
-                    <p>Stevenson college.</p>
+                    <p>Congrats on choosing Stevenson College, this college was founded in 1966 and is named after Adali E. Stevenson.
+                        Stevenson College’s theme is Self and society, focusing on understanding oneself,
+                        actively participating in your community and thinking globally.
+                    </p>
+                    <h3>Core Course:</h3>
+                    <p>There are two required core courses that freshmen must enroll in, one is taught
+                        during the fall quarter while the other is taught during the winter quarter. This
+                        core course focuses on the relationship between self and society teaching students
+                        about diverse historical, cultural, and intellectual traditions. </p>
+                    <h3>Environment: </h3>
+                    <p>Stevenson College is located in one of the most beautiful places on campus, with
+                        the east field nearby and a beautiful view of the ocean. This college is a great
+                        hang-out stop. The athletics and recreational fitness center is also located nearby
+                        with a large variety of gym equipment, yoga classes, and more! Stevenson’s location is
+                        perfect for students who want to hit the gym. Another selling point of this college is
+                        the amazing Stevenson Coffee House which sells amazing sandwiches, coffee and so much
+                        more! A bustling coffee shop that never fails to impress. Stevenson College has dorms,
+                        apartments, and a themed living space, the Rosa Parks African Theme House or R.PAATH.
+                        This themed housing is a part of the ABC housing on campus that is predominantly African,
+                        Black, and Caribbean students. This housing is great for students who are passionate about
+                        political, social, cultural, and environmental issues of African, Black, and Caribbean
+                        people. This themed living space is perfect for students who share these interests and wish
+                        to be surrounded by peers with the same interests.  </p>
+                    <h3>Our Thoughts:</h3>
+                    <p>Stevenson has an absolutely beautiful view and an amazing environment, there are many
+                        types of people here however there is a large amount of “gym bros” due to how close the gym is. </p>
                 </div>
             )
         },
         {
             content: (
                 <div>
-                    <h2>Step 2: More details about Topic 1</h2>
-                    <p>This is the second step of Topic 1.</p>
+                    <h2>Next question</h2>
+                    <p>Placeholder</p>
                 </div>
             )
         },
         {
             content: (
                 <div>
-                    <h2>Step 3: Conclusion of Topic 1</h2>
-                    <p>This is the third step of Topic 1.</p>
+                    <h2>Next question</h2>
+                    <p>Placeholder</p>
                 </div>
             )
         },
@@ -37,24 +62,34 @@ const sections = {
         {
             content: (
                 <div>
-                    <h2>Welcome to Cowell College</h2>
-                    <p>This is the first step of Topic 2.</p>
+                    <h2>Welcome to Cowell College!</h2>
+                    <img class="cowell"
+                        src="./img/cowelldorms.jpg"
+                        alt="Cowell College dorms at UCSC">
+                    </img>
+                    <p>Placeholder</p>
+                    <h3>Core Course:</h3>
+                    <p>Information about core course placeholder</p>
+                    <h3>Environment: </h3>
+                    <p>Information about environment placeholder</p>
+                    <h3>Our Thoughts:</h3>
+                    <p>Information out thoughts placeholder</p>
                 </div>
             )
         },
         {
             content: (
                 <div>
-                    <h2>Step 2: More details about Topic 2</h2>
-                    <p>This is the second step of Topic 2.</p>
+                    <h2>Next question</h2>
+                    <p>Placeholder</p>
                 </div>
             )
         },
         {
             content: (
                 <div>
-                    <h2>Step 3: Conclusion of Topic 2</h2>
-                    <p>This is the third step of Topic 2.</p>
+                    <h2>Next question</h2>
+                    <p>Placeholder</p>
                 </div>
             )
         },
@@ -63,24 +98,34 @@ const sections = {
         {
             content: (
                 <div>
-                    <h2>Welcome to Merrill College</h2>
-                    <p>This is the first step of Topic 3.</p>
+                    <h2>Welcome to Merrill College!</h2>
+                    <img class="merrill"
+                        src="./img/"
+                        alt="">
+                    </img>
+                    <p>Placeholder</p>
+                    <h3>Core Course:</h3>
+                    <p>Information about core course placeholder</p>
+                    <h3>Environment: </h3>
+                    <p>Information about environment placeholder</p>
+                    <h3>Our Thoughts:</h3>
+                    <p>Information out thoughts placeholder</p>
                 </div>
             )
         },
         {
             content: (
                 <div>
-                    <h2>Step 2: More details about Topic 3</h2>
-                    <p>This is the second step of Topic 3.</p>
+                    <h2>Next question</h2>
+                    <p>Placeholder</p>
                 </div>
             )
         },
         {
             content: (
                 <div>
-                    <h2>Step 3: Conclusion of Topic 3</h2>
-                    <p>This is the third step of Topic 3.</p>
+                    <h2>Next question</h2>
+                    <p>Placeholder</p>
                 </div>
             )
         },
@@ -89,24 +134,34 @@ const sections = {
         {
             content: (
                 <div>
-                    <h2>Welcome to Crown College</h2>
-                    <p>This is the first step of Topic 3.</p>
+                    <h2>Welcome to Crown College!</h2>
+                    <img class="crown"
+                        src="./img/"
+                        alt="">
+                    </img>
+                    <p>Placeholder</p>
+                    <h3>Core Course:</h3>
+                    <p>Information about core course placeholder</p>
+                    <h3>Environment: </h3>
+                    <p>Information about environment placeholder</p>
+                    <h3>Our Thoughts:</h3>
+                    <p>Information out thoughts placeholder</p>
                 </div>
             )
         },
         {
             content: (
                 <div>
-                    <h2>Step 2: More details about Topic 3</h2>
-                    <p>This is the second step of Topic 3.</p>
+                    <h2>Next question</h2>
+                    <p>Placeholder</p>
                 </div>
             )
         },
         {
             content: (
                 <div>
-                    <h2>Step 3: Conclusion of Topic 3</h2>
-                    <p>This is the third step of Topic 3.</p>
+                    <h2>Next question</h2>
+                    <p>Placeholder</p>
                 </div>
             )
         },
@@ -115,24 +170,34 @@ const sections = {
         {
             content: (
                 <div>
-                    <h2>Welcome to College Nine</h2>
-                    <p>This is the first step of Topic 3.</p>
+                    <h2>Welcome to College Nine!</h2>
+                    <img class="nine"
+                        src="./img/"
+                        alt="">
+                    </img>
+                    <p>Placeholder</p>
+                    <h3>Core Course:</h3>
+                    <p>Information about core course placeholder</p>
+                    <h3>Environment: </h3>
+                    <p>Information about environment placeholder</p>
+                    <h3>Our Thoughts:</h3>
+                    <p>Information out thoughts placeholder</p>
                 </div>
             )
         },
         {
             content: (
                 <div>
-                    <h2>Step 2: More details about Topic 3</h2>
-                    <p>This is the second step of Topic 3.</p>
+                    <h2>Next question</h2>
+                    <p>Placeholder</p>
                 </div>
             )
         },
         {
             content: (
                 <div>
-                    <h2>Step 3: Conclusion of Topic 3</h2>
-                    <p>This is the third step of Topic 3.</p>
+                    <h2>Next question</h2>
+                    <p>Placeholder</p>
                 </div>
             )
         },
@@ -141,24 +206,34 @@ const sections = {
         {
             content: (
                 <div>
-                    <h2>Welcome to John R. Lewis College</h2>
-                    <p>This is the first step of Topic 3.</p>
+                    <h2>Welcome to John R. Lewis College!</h2>
+                    <img class="jrl"
+                        src="./img/"
+                        alt="">
+                    </img>
+                    <p>Placeholder</p>
+                    <h3>Core Course:</h3>
+                    <p>Information about core course placeholder</p>
+                    <h3>Environment: </h3>
+                    <p>Information about environment placeholder</p>
+                    <h3>Our Thoughts:</h3>
+                    <p>Information out thoughts placeholder</p>
                 </div>
             )
         },
         {
             content: (
                 <div>
-                    <h2>Step 2: More details about Topic 3</h2>
-                    <p>This is the second step of Topic 3.</p>
+                    <h2>Next question</h2>
+                    <p>Placeholder</p>
                 </div>
             )
         },
         {
             content: (
                 <div>
-                    <h2>Step 3: Conclusion of Topic 3</h2>
-                    <p>This is the third step of Topic 3.</p>
+                    <h2>Next question</h2>
+                    <p>Placeholder</p>
                 </div>
             )
         },
@@ -167,24 +242,34 @@ const sections = {
         {
             content: (
                 <div>
-                    <h2>Welcome to Kresge College</h2>
-                    <p>This is the first step of Topic 3.</p>
+                    <h2>Welcome to Kresge College!</h2>
+                    <img class="kresge"
+                        src="./img/"
+                        alt="">
+                    </img>
+                    <p>Placeholder</p>
+                    <h3>Core Course:</h3>
+                    <p>Information about core course placeholder</p>
+                    <h3>Environment: </h3>
+                    <p>Information about environment placeholder</p>
+                    <h3>Our Thoughts:</h3>
+                    <p>Information out thoughts placeholder</p>
                 </div>
             )
         },
         {
             content: (
                 <div>
-                    <h2>Step 2: More details about Topic 3</h2>
-                    <p>This is the second step of Topic 3.</p>
+                    <h2>Next question</h2>
+                    <p>Placeholder</p>
                 </div>
             )
         },
         {
             content: (
                 <div>
-                    <h2>Step 3: Conclusion of Topic 3</h2>
-                    <p>This is the third step of Topic 3.</p>
+                    <h2>Next question</h2>
+                    <p>Placeholder</p>
                 </div>
             )
         },
@@ -193,24 +278,34 @@ const sections = {
         {
             content: (
                 <div>
-                    <h2>Welcome to Porter College</h2>
-                    <p>This is the first step of Topic 3.</p>
+                    <h2>Welcome to Porter College!</h2>
+                    <img class="porter"
+                        src="./img/"
+                        alt="">
+                    </img>
+                    <p>Placeholder</p>
+                    <h3>Core Course:</h3>
+                    <p>Information about core course placeholder</p>
+                    <h3>Environment: </h3>
+                    <p>Information about environment placeholder</p>
+                    <h3>Our Thoughts:</h3>
+                    <p>Information out thoughts placeholder</p>
                 </div>
             )
         },
         {
             content: (
                 <div>
-                    <h2>Step 2: More details about Topic 3</h2>
-                    <p>This is the second step of Topic 3.</p>
+                    <h2>Next question</h2>
+                    <p>Placeholder</p>
                 </div>
             )
         },
         {
             content: (
                 <div>
-                    <h2>Step 3: Conclusion of Topic 3</h2>
-                    <p>This is the third step of Topic 3.</p>
+                    <h2>Next question</h2>
+                    <p>Placeholder</p>
                 </div>
             )
         },
@@ -219,24 +314,34 @@ const sections = {
         {
             content: (
                 <div>
-                    <h2>Welcome to Rachel Carson College</h2>
-                    <p>This is the first step of Topic 3.</p>
+                    <h2>Welcome to Rachel Carson College!</h2>
+                    <img class="rcc"
+                        src="./img/"
+                        alt="">
+                    </img>
+                    <p>Placeholder</p>
+                    <h3>Core Course:</h3>
+                    <p>Information about core course placeholder</p>
+                    <h3>Environment: </h3>
+                    <p>Information about environment placeholder</p>
+                    <h3>Our Thoughts:</h3>
+                    <p>Information out thoughts placeholder</p>
                 </div>
             )
         },
         {
             content: (
                 <div>
-                    <h2>Step 2: More details about Topic 3</h2>
-                    <p>This is the second step of Topic 3.</p>
+                    <h2>Next question</h2>
+                    <p>Placeholder</p>
                 </div>
             )
         },
         {
             content: (
                 <div>
-                    <h2>Step 3: Conclusion of Topic 3</h2>
-                    <p>This is the third step of Topic 3.</p>
+                    <h2>Next question</h2>
+                    <p>Placeholder</p>
                 </div>
             )
         },
@@ -245,24 +350,34 @@ const sections = {
         {
             content: (
                 <div>
-                    <h2>Welcome to Oaks College</h2>
-                    <p>This is the first step of Topic 3.</p>
+                    <h2>Welcome to Oakes College!</h2>
+                    <img class="oakes"
+                        src="./img/"
+                        alt="">
+                    </img>
+                    <p>Placeholder</p>
+                    <h3>Core Course:</h3>
+                    <p>Information about core course placeholder</p>
+                    <h3>Environment: </h3>
+                    <p>Information about environment placeholder</p>
+                    <h3>Our Thoughts:</h3>
+                    <p>Information out thoughts placeholder</p>
                 </div>
             )
         },
         {
             content: (
                 <div>
-                    <h2>Step 2: More details about Topic 3</h2>
-                    <p>This is the second step of Topic 3.</p>
+                    <h2>Next question</h2>
+                    <p>Placeholder</p>
                 </div>
             )
         },
         {
             content: (
                 <div>
-                    <h2>Step 3: Conclusion of Topic 3</h2>
-                    <p>This is the third step of Topic 3.</p>
+                    <h2>Next question</h2>
+                    <p>Placeholder</p>
                 </div>
             )
         },
@@ -302,7 +417,7 @@ function Game() {
                     <button onClick={() => setCurrentSection('section7')}>Kresge College</button>
                     <button onClick={() => setCurrentSection('section8')}>Porter College</button>
                     <button onClick={() => setCurrentSection('section9')}>Rachel Carson College</button>
-                    <button onClick={() => setCurrentSection('section10')}>Oaks College</button>
+                    <button onClick={() => setCurrentSection('section10')}>Oakes College</button>
                 </div>
             ) : (
                 // buttons! (patrick helped) 
